ae950d678d8d4412888b39ab4d5c41dcb88c9c0e
Rhys van der Waerden <rhys.vdw@gmail.com>
2013-09-03 20:00:27 +1000

    Initial commit. Added basic functionality.

---
+++ Hierarchy.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnityBasics
+{
+
+public static class Hierarchy
+{
+    public static IEnumerable<Transform> Children( this Transform transform )
+    {
+        foreach( Transform child in transform )
+        {
+            yield return child;
+        }
+    }
+
+    public static IEnumerable<Transform> Descendants( this Transform transform )
+    {
+        foreach( var child in transform.Children() )
+        {
+            yield return child;
+            foreach( var grandChild in child.Children() )
+            {
+                yield return grandChild;
+            }
+        }
+    }
+
+    public static IEnumerable<Transform> Ancestors( this Transform transform )
+    {
+        while( transform.parent != null )
+        {
+            transform = transform.parent;
+            yield return transform;
+        }
+    }
+
+    public static IEnumerable<Transform> SelfChildren( this Transform transform )
+    {
+        return new [] { transform }.Concat( transform.Children() );
+    }
+
+    public static IEnumerable<Transform> SelfDescendants( this Transform transform )
+    {
+        return new [] { transform }.Concat( transform.Descendants() );
+    }
+
+    public static IEnumerable<Transform> SelfAncestors( this Transform transform )
+    {
+        while( transform != null )
+        {
+            yield return transform;
+            transform = transform.parent;
+        }
+    }
+}
+
+}
